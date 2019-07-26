fb585d51802963c51b05467f94e9870a6755b734
dyspace <yevhenii.kukhol@gmail.com>
2019-07-26 10:18:11 +0300

    without exeption

--- WeatherClass.php
+++ WeatherClass.php
@@ -6,7 +6,7 @@ class Weather{
     private $lat;
     private $lon;
     private $url = "api.openweathermap.org/data/2.5/weather?";
-    private $api_key = "6a64e91caaef7e9663dc23664065acb4";
+    private $api_key = api_key;
 
     private $response;
     private $readyUrl;
